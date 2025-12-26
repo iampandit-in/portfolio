903ed535550d9d00352697be9867960f167cf0e0
Pandit Pawar <iampandit.in@gmail.com>
2025-12-26 22:30:59 +0530

    add @vercel/analytics integration to layout and update package dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -40,6 +41,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <Analytics />
           <main className="text-foreground/90 mx-auto mt-6 max-w-3xl p-4">
             {children}
           </main>
